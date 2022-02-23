00e836503d1181d892021aad173a6cce912087ce
María José <maria.jose.berrocal.vega@gmail.com>
2022-02-23 07:44:47 -0500

    nuevo trabajo

---
+++ Taller 1.js
@@ -0,0 +1,9 @@
+/*
+Archivo de práctica y pruebas de JavaScript
+*/
+// Taller 1, Fecha: 23/02/2022, Realizado por: María José Berrocal//
+
+var nombreVariable 
+var gradoEstudiante = "10A";
+console.log (edadUsuario);
+var edad= 17
---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("Hola Mundo");
+// comentario en linea
+/*
+comentario
+de
+varias
+lineas
+*/
+
+//Variables
+var primerVariable;
+let edadUsuario;
+//Declarar Variables
+var nombreVariable
+//Crear Variable sin asignar valor
+/*
+Inicializar variables:
+asignar un valor al momento de crear la variable
+*/
+var gradoEstudiante="10A";
+/*
+Asignar Valor:
+igualar una variable existente a un nuevo valor
+*/
+edadUsuario=16;
+//Asignar valor desde otra variable//
+console.log(edadUsuario);
+var edad=17;
+edadUsuario=edad;
+console.log(edadUsuario);
+/*
+Asignar valor desde teclado:
+existe dos formas de hacerlo, la primera es hacerlo por ventana emergente y la segunda es por medio de un formulario en html
+*/
+var valor = prompt ("ingrese un valor");
+console.log ("la variable vale " + valor);
+
+/* 
+Constante:
+Son valores que no pueden ser cambios durante la ejecución del código. Las constantes siempre deben ser inicializadas
+*/
+const PI = 3.1416;
+//imprimir por consola//
+//simple
+console.log ("...");
+console.log  (valor);
+//concalenado
+console.log ("..." + valor);
