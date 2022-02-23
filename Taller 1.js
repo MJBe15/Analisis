5091d2aec82085c1ae80400b578d635c8d81ff84
María José <maria.jose.berrocal.vega@gmail.com>
2022-02-23 08:01:22 -0500

    nuevo trabajo

--- Taller 1.js
+++ Taller 1.js
@@ -3,8 +3,6 @@ Archivo de práctica y pruebas de JavaScript
 */
 // Taller 1, Fecha: 23/02/2022, Realizado por: María José Berrocal //
 
-var nombreVariable
+var nombreVariable;
 var gradoEstudiante = "10A";
-console.log (edadUsuario);
-var edad= 17;
-edadUsuario=edad;
+var valor = prompt ("Edad 15");
