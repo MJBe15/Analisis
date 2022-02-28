e898d5924c94c856ece5067df4013ab67420a17a
María José <maria.jose.berrocal.vega@gmail.com>
2022-02-28 07:13:10 -0500

    trabajo nuevo

--- Taller 1.js
+++ Taller 1.js
@@ -4,6 +4,6 @@ Archivo de práctica y pruebas de JavaScript
 // Taller 1, Fecha: 23/02/2022, Realizado por: María José Berrocal //
 
 var nombreVariable=MaríaJoséBerrocal; //String
-var gradoEstudiante = "10A";  //String
+var gradoEstudiante = "10B";  //String
 var valor = prompt ("ingrese un valor"); //int
-console.log ("la variable vale " + valor);
+console.log ("El estudiante María José del grado 10A tiene" + 15 , "años");
