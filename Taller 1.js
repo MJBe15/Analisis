4ca49605bf832b636afbe3339018b23f8b5679c2
María José <maria.jose.berrocal.vega@gmail.com>
2022-02-28 07:30:00 -0500

    nuevo trabajo

--- Taller 1.js
+++ Taller 1.js
@@ -6,5 +6,5 @@ Archivo de práctica y pruebas de JavaScript
 var nombreVariable=MaríaJoséBerrocal; //String
 var gradoEstudiante = "10B";  //String
 var valor = prompt ("ingrese un valor"); //int
-console.log ("la edad es" + 15);
+console.log ("la variable vale" + 15);
 console.log("El estudiante María José del grado 10A tiene 15 años")
