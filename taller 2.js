a6f0ae34ad20d3569fefce1d9193b53d2d043841
María José <maria.jose.berrocal.vega@gmail.com>
2022-03-09 06:53:01 -0500

    Solución taller 2

---
+++ taller 2.js
@@ -0,0 +1,6 @@
+//Halle el área de un cuadrado y volumen de un cubo
+var area1 =prompt("ingrese la medida de un lado del cuadrado")
+var area2 =prompt ("ingrese la medida del otro lado del cuadrado")
+var cuadrado;
+var cuadrado = area1 * area2;
+console.log (cuadrado);
