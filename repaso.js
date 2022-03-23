5ca79c1fd956457a20cb64008fdf4f3aa4a37eb4
María José <maria.jose.berrocal.vega@gmail.com>
2022-03-23 06:59:57 -0500

    nuevo trabajo

---
+++ repaso.js
@@ -0,0 +1,38 @@
+//primer punto
+
+//crear variables
+var metros;
+var kilometros;
+
+//solicitar valor al usuario
+metros =prompt("por favor ingrese un valor en metros");
+
+//operación
+kilometros = metros / 1000;
+
+//mostrar mensaje
+console.log ("la equivalencia de"+  metros + "m en km es"+kilometros+"km");
+
+//segundo punto
+
+//crear variables
+var distancia
+var litrosGastados
+
+//solicitar la distancia al usuario
+distancia =prompt ("por favor ingrese la distancia que recorre el avión")
+
+//operación
+litrosGastados = distancia *290/100;
+
+//mostrar mensaje
+console.log (" El total de combustible que consume un avión de acuerdo a la distancia" + distancia + " del viaje es" + litrosGastados +"L" );
+
+//tercer punto
+
+var kilolitros
+var litrosGastados
+
+kilolitros = litrosGastados/0,001;
+
+console.log ("el resultado anterior convertido en Kl es" + kilolitros);
